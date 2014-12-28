a9d3ab74c7f44f8be92e210ccccf7704260b4185
Patrick Arnesen <patrick.arnesen@gamil.com>
2014-12-28 11:38:35 -0800

    observable variable

--- web/elements/simple-element/simple_element.dart
+++ web/elements/simple-element/simple_element.dart
@@ -3,7 +3,15 @@ import 'package:polymer/polymer.dart';
 
 @CustomTag('simple-element')
 class SimpleElement extends PolymerElement {
-  SimpleElement.created() : super.created() {}
+    SimpleElement.created() : super.created() {}
+      
+    @observable String content = "initial content";
+    int _clickCount = 0;
+  
+    void onBtnClick() {
+        _clickCount++;
+        content = "clicked ${_clickCount} times";
+    }
 }
 
 
