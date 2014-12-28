1b553b336f83b2dffde1649633835f19f96c65f5
Patrick Arnesen <patrick.arnesen@gamil.com>
2014-12-28 12:00:58 -0800

    modify dom elements from dart

--- web/elements/simple-element/simple_element.dart
+++ web/elements/simple-element/simple_element.dart
@@ -1,5 +1,5 @@
 import 'package:polymer/polymer.dart';
-
+import 'dart:html';
 
 @CustomTag('simple-element')
 class SimpleElement extends PolymerElement {
@@ -7,10 +7,16 @@ class SimpleElement extends PolymerElement {
       
     @observable String content = "initial content";
     int _clickCount = 0;
+    ButtonElement button;
+    
+    void attached() {
+        button = $['btn'];
+    }
   
     void onBtnClick() {
         _clickCount++;
         content = "clicked ${_clickCount} times";
+        button.text = "Click Me Again!";
     }
 }
 
